3405aeb25d2b3be01dd20749d92df1cfc0bda715
Aayam Adhikari <aayam@Aayams-MacBook-Air.local>
2023-07-24 16:16:54 +0545

    Initial Commit

---
+++ StarterGuide/StarterGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterGuideApp.swift
+//  StarterGuide
+//
+//  Created by Aayam Adhikari on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct StarterGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
